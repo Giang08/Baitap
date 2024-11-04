a441d6e316e2f8f0c4273e86690a14ce24eb37c3
Giang08 <gnguyen0724@gmail.com>
2024-11-04 19:48:33 +0700

    suadoi

--- w3.cpp
+++ w3.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 //Bai 1
-int fibonacci( int n)
+int fibonacci( int h)
 {
-    if (n == 1 || n == 2) {
+    if (h == 1 || h == 2) {
         return 1;
     } else {
-        return fibonacci (n - 1) + fibonacci (n - 2);
+        return fibonacci (h - 1) + fibonacci (h - 2);
     } return 0;
 }
 
@@ -30,16 +30,36 @@ int BCNN (int a, int b)
     return (a * b) / UCLN(a,b);
 }
 
+int Tong (int m)
+{
+    long long s;
+    s = (m * (m + 1) * (2*m + 1)) / 6;
+    return s;
+}
 
+int GiaiThua (int n) {
+    int s = 1;
+        for (int i = 1; i <= n; i++) 
+        s *= i;
+        return s;
+}
+
+int ToHop (int n, int k) {
+    if (n == k || k == 0) 
+    return 1;
+    else {
+        return (GiaiThua(n) /  (GiaiThua(k) * GiaiThua(n - k))); 
+    }
+}
 
 
 int main ()
 {
-    int n;
-    cout << "Nhap so n: \n";
-    cin >> n;
-    long long fibo = fibonacci (n);
-    cout << "So fibonacii thu " << n <<" la " << fibo <<"\n";
+    int h;
+    cout << "Nhap so h: \n";
+    cin >> h;
+    long long fibo = fibonacci (h);
+    cout << "So fibonacii thu " << h <<" la " << fibo <<"\n";
     int a,b;
     cout <<"Hay nhap 2 so a,b: ";
     cin >> a;
@@ -48,6 +68,19 @@ int main ()
     int bcnn = BCNN(a,b);
     cout <<"UCLN cua 2 so la: " << ucln <<"\n";
     cout <<"BCNN cua 2 so la: " << bcnn <<"\n";
+    int m;
+    cout << "Nhap so m: \n";
+    cin >> m;
+    int tong = Tong(m);
+    cout << "Tong binh phuong lien tiep cua " << m << " so la: " << tong << endl;
+    int n,k;
+    cout <<"Nhap so n va k: " << endl;
+    cin >> n;
+    cin >> k;
+    cout <<"To hop chap " << k <<" cua " << n <<" la: ";
+    int TH = ToHop(n,k);
+    cout << TH << endl;
+
     
  
 }
